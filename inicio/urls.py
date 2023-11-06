b777049e3f1d1c69160acff0a6da0654e0254829
Natalia Chiara <natalia@contenidos-digitales.com>
2023-11-06 00:02:40 -0300

    wip

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from inicio.views import inicio, paletas
+from inicio.views import inicio, paletas, crear_paleta
 
 urlpatterns = [
-    path('', inicio),
-    path('paletas/', paletas)
+    path('', inicio, name='inicio'),
+    path('paletas/', paletas, name='paletas'),
+    path('paletas/crear/', crear_paleta, name='crear_paleta')
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from inicio.models import Paleta
+from inicio.forms import CrearPaletaFormulario
 
 def inicio(request):
     return render(request, 'inicio/inicio.html', {})
 
 def paletas(request):
-    paleta = Paleta(marca='Wilson', descripcion='paleta de bela', anio = 2022)
-    paleta.save() #esto lo guarda en la base de datos cuando se accede a la pagina '/paletas'
-    return render(request, 'inicio/paletas.html', {'paleta': paleta})
+    marca_a_buscar = request.GET.get('marca')
+    if marca_a_buscar:
+        listado_de_paletas = Paleta.objects.filter(marca__icontains = marca_a_buscar)
+    else:
+        listado_de_paletas = Paleta.objects.all()
+    return render(request, 'inicio/paletas.html', {'listado_de_paletas': listado_de_paletas})
+
+def crear_paleta(request):
+    # if request.method == 'POST':
+    #     marca = request.POST.get('marca')
+    #     descripcion = request.POST.get('descripcion')
+    #     anio = request.POST.get('anio')
+    #     paleta = Paleta(marca=marca, descripcion=descripcion, anio = anio)
+    #     paleta.save() HTML
+
+    if request.method == 'POST':
+        formulario = CrearPaletaFormulario(request.POST)
+        if formulario.is_valid():
+            info_limpia = formulario.cleaned_data
+            marca = info_limpia.get('marca')
+            descripcion = info_limpia.get('descripcion')
+            anio = info_limpia.get('anio')
+            paleta = Paleta(marca=marca.lower(), descripcion=descripcion, anio=anio)
+            paleta.save() #esto lo guarda en la base de datos cuando se accede a la pagina '/paletas/crear'
+            return redirect('paletas')
+        else:
+            return render(request, 'inicio/crear_paleta.html' , {'formulario': formulario})
+
+    formulario = CrearPaletaFormulario()
+    return render(request, 'inicio/crear_paleta.html' , {'formulario': formulario})
