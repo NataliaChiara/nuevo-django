9e72a83a6298e7610108adc4d1420ec1627250b7
Natalia Chiara <natalia@contenidos-digitales.com>
2023-11-06 09:17:54 -0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # nuevo-django
+
+1 - Para correr el proyecto se necesita activar el entorno con el comando
+
+. env/Scripts/activate (desactivarlo con "deactivate")
+
+2 - Para correr el servidor se necesita activarlo con el comando
+
+python manage.py runserver
+
+3 - Se accede al sitio a traves de
+
+http://127.0.0.1:8000/
+
+4 - Si se agrega alguna dependencia, actualizar el requirements.txt con el comando
+
+pip freeze > requirements.txt
--- inicio/views.py
+++ inicio/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render, redirect
 from inicio.models import Paleta
-from inicio.forms import CrearPaletaFormulario
+from inicio.forms import CrearPaletaFormulario, BusquedaPaletaFormulario
 
 def inicio(request):
     return render(request, 'inicio/inicio.html', {})
 
 def paletas(request):
-    marca_a_buscar = request.GET.get('marca')
-    if marca_a_buscar:
-        listado_de_paletas = Paleta.objects.filter(marca__icontains = marca_a_buscar)
-    else:
-        listado_de_paletas = Paleta.objects.all()
-    return render(request, 'inicio/paletas.html', {'listado_de_paletas': listado_de_paletas})
+    # marca_a_buscar = request.GET.get('marca')
+    # if marca_a_buscar:
+    #     listado_de_paletas = Paleta.objects.filter(marca__icontains = marca_a_buscar)
+    # else:
+    #     listado_de_paletas = Paleta.objects.all() FORMA HTML
 
+    formulario = BusquedaPaletaFormulario(request.GET)
+    if formulario.is_valid():
+        marca_a_buscar = formulario.cleaned_data.get('marca')
+        listado_de_paletas = Paleta.objects.filter(marca__icontains = marca_a_buscar)
+
+    formulario = BusquedaPaletaFormulario()
+    return render(request, 'inicio/paletas.html', {'formulario': formulario, 'listado_de_paletas': listado_de_paletas}) 
+    
 def crear_paleta(request):
     # if request.method == 'POST':
     #     marca = request.POST.get('marca')
     #     descripcion = request.POST.get('descripcion')
     #     anio = request.POST.get('anio')
     #     paleta = Paleta(marca=marca, descripcion=descripcion, anio = anio)
-    #     paleta.save() HTML
+    #     paleta.save() FORMA HTML
 
     if request.method == 'POST':
         formulario = CrearPaletaFormulario(request.POST)
